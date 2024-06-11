d6536ebc2c2084c497977ba0ddef0f597b232aa3
Paul Chambless <pc7900@gmail.com>
2024-06-11 19:00:54 +0000

    jsIngr add break after each query in case condition.

--- pages/Ingredients/jsobjects/jsIngr/jsIngr.js
+++ pages/Ingredients/jsobjects/jsIngr/jsIngr.js
@@ -3,10 +3,13 @@ editModeRun: async (mode) => {
 	switch(mode) {
 		case 'Add':
 			await ingr_Add.run();
+			break;
 		case 'Edit':
 			await ingr_Edit.run();
+			break;
 		case 'Delete':
 			await ingr_Delete.run();
+			break;
 		default:
 			console.log("Invalid mode");
 		}
